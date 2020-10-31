ac5c648f1e8ae5aec6258aacc1ea56815aa6fd08
Kickbuttowaski <promods96@gmail.com>
2020-10-31 17:14:47 +0530

    server file added

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(
+  "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"
+);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.post("/sendmail", (req, res) => {
+  const msg = {
+    to: "chottu64@gmail.com", // Change to your recipient
+    from: "promods96@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  res.send("success").status(200);
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
