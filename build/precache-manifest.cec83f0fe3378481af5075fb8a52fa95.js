b5a866c3c0834aeb4ce1b49363a07aafa3b2322b
Kickbuttowaski <promods96@gmail.com>
2020-10-31 17:39:26 +0530

    build addded

---
+++ build/precache-manifest.cec83f0fe3378481af5075fb8a52fa95.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10e67eddc4ebad7a389e2d3ce830a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e44087de383fc87481",
+    "url": "/static/css/main.6f6f2126.chunk.css"
+  },
+  {
+    "revision": "09025e138eff619cf4d9",
+    "url": "/static/js/2.4bcad269.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.4bcad269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e44087de383fc87481",
+    "url": "/static/js/main.5b50335b.chunk.js"
+  },
+  {
+    "revision": "5527f393b10d7459bc65",
+    "url": "/static/js/runtime-main.27f699ad.js"
+  },
+  {
+    "revision": "c14e5339ea4c825a4d43ed39fbd06775",
+    "url": "/static/media/005-orthopedist.c14e5339.svg"
+  },
+  {
+    "revision": "13b86eb901a6815b4e2b8220951c259c",
+    "url": "/static/media/006-therapist.13b86eb9.svg"
+  },
+  {
+    "revision": "6f4a7e93ca760ff923c566f50749fb0d",
+    "url": "/static/media/009-psychiatrist.6f4a7e93.svg"
+  },
+  {
+    "revision": "ae420de95229bb0dc83108946a97efa4",
+    "url": "/static/media/015-gynecologist.ae420de9.svg"
+  },
+  {
+    "revision": "95c4991042bc4694978c33a08db4d4d7",
+    "url": "/static/media/018-pediatrician.95c49910.svg"
+  },
+  {
+    "revision": "52c5f56b6122116aef74d8396aef4f07",
+    "url": "/static/media/021-oncologist.52c5f56b.svg"
+  },
+  {
+    "revision": "73079c625ce8cfd9994ef934897d1852",
+    "url": "/static/media/029-nurse.73079c62.svg"
+  },
+  {
+    "revision": "02a03d94ed5db7b0d9c383c65b8ad16f",
+    "url": "/static/media/038-nurse.02a03d94.svg"
+  },
+  {
+    "revision": "a4b9d7a52ee06fc0e1c25ae5c4ea4bc1",
+    "url": "/static/media/039-gynecologist.a4b9d7a5.svg"
+  },
+  {
+    "revision": "6f29fdc15e65e9b81173b6a861cfde46",
+    "url": "/static/media/049-surgeon.6f29fdc1.svg"
+  },
+  {
+    "revision": "3e7a1c3b69c0c5eec478e6a204452d59",
+    "url": "/static/media/cardio.3e7a1c3b.svg"
+  },
+  {
+    "revision": "a1799cc1f1fc400e512f4d9405c4207a",
+    "url": "/static/media/logo.a1799cc1.jpeg"
+  },
+  {
+    "revision": "5439eb7cfd0d8d6be9b8cc66b5c91879",
+    "url": "/static/media/mainImg.5439eb7c.jpg"
+  }
+]);
