bda1988455fde8c98c91ac50d65ee7438916090b
Kickbuttowaski <promods96@gmail.com>
2020-10-31 18:33:12 +0530

    store API key in env variable

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(
-  "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"
-);
+sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 app.use(express.static(path.join(__dirname, "build")));
 
